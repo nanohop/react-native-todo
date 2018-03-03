5a8c10f83707634c782e13304e541a544dbdd4a0
Chris Achard <chrisachard@gmail.com>
2018-03-02 23:46:37 -0500

    Lesson 13

--- TodoList/src/components/AddTodo.js
+++ TodoList/src/components/AddTodo.js
@@ -2,24 +2,62 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  StatusBar
+  StatusBar,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity
 } from 'react-native';
 
 export default class AddTodo extends Component {
+
+  state = {
+    text: ''
+  }
+
+  onAdd = () => {
+    console.log("Text", this.state.text)
+  }
 
   render() {
     return (
       <View style={{flex: 1}}>
         <StatusBar barStyle="dark-content" />
-        <Text style={{
-          fontSize: 20,
-          textAlign: 'center',
-          padding: 20
-        }}>
-          Add Todo
-        </Text>
+
+        <TextInput 
+          style={styles.input} 
+          onChangeText={text => this.setState({ text }) }
+          value={this.state.text}
+        />
+
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={this.onAdd}
+        >
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 
 }
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc'
+  },
+  button: {
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: '#0066cc',
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff'
+  }
+})
+
