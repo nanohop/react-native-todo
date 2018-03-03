976a67a8f34f3a4ab07d2ff92cc9136fdf83a4cf
Chris Achard <chrisachard@gmail.com>
2018-03-02 23:18:34 -0500

    Lesson 6

--- TodoList/src/components/TodoItem.js
+++ TodoList/src/components/TodoItem.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 
 export default class TodoItem extends Component {
 
+  state = {
+    completed: false
+  }
+
+  toggleTodo = () => {
+    this.setState({
+      completed: !this.state.completed
+    })
+  }
+
   render() {
-    return <Text style={styles.item}>{this.props.title}</Text>
+    return (
+      <TouchableOpacity onPress={this.toggleTodo}>
+        <Text style={[styles.item, {
+          backgroundColor: (this.state.completed ? 'grey' : 'transparent')
+        }]}>
+          {this.props.title}
+        </Text>
+      </TouchableOpacity>
+    )
   }
 
 }
